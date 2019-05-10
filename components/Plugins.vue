30af9c7161894ec3ee6d4583da1276156430f554
jarrodyellets <jarrod524@gmail.com>
2019-05-10 11:39:45 +0200

    Plugins page

---
+++ components/Plugins.vue
@@ -0,0 +1,30 @@
+<template lang="md">
+  <div class="markdown-wrapper" v-html="page"></div>
+</template>
+
+<script>
+import page from '../static/lib/plugins.md';
+
+export default {
+    computed: {
+      page() {
+        return page
+      }
+    }
+};
+
+</script>
+
+<style lang="scss">
+@import "../assets/styles/main.scss";
+@import "../assets/styles/markdown.scss";
+
+.markdown-wrapper {
+  position: relative;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 20px 0 20px 100px;
+}
+
+</style>
---
+++ pages/plugins.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <PluginsNav />
+    <div class="tutorial-markdown-window">
+      <Plugins />
+    </div>
+  </div>
+</template>
+
+<script>
+import Plugins from "~/components/Plugins.vue";
+import PluginsNav from "~/components/Navs/PluginsNav.vue";
+export default {
+  components: {
+    Plugins,
+    PluginsNav
+  }
+};
+</script>
+
+<style lang="scss">
+.spacer {
+  width: 100%;
+  height: 96px;
+}
+
+.container {
+  position: relative;
+  width: 100%;
+  max-width: 1260px;
+  margin: 96px auto 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+
+.tutorial-markdown-window {
+  position: relative;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
