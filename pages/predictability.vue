47137966a638b01100a74305dfe99cdc9b75ead5
jarrodyellets <jarrod524@gmail.com>
2019-05-19 00:10:06 +0200

    Added predictability

---
+++ pages/predictability.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="page-wrapper">
+    <div class="predict-upper-wrapper">
+      <h2 class="predict-heading">Keeps Large Distributed Teams on the Same Page</h2>
+      <div class="predict-content">
+        hapi was designed from the group-up to <span class="bold">support large, distributed, and remote teams</span>  – allowing
+        them to effectively work together on common code. No more toe stepping.
+      </div>
+    </div>
+    <div class="predict-middle-wrapper">
+      <h3 class="predict-heading">Say goodbye to middleware hell.</h3>
+      <div class="predict-content">
+        hapi was the first node framework (and in some ways, still the only) to provide <span class="bold">strong guarantees</span> .
+        The order in which parallel or unrelated components are configured should never produce
+        unexpected results.
+      </div>
+    </div>
+    <div class="predict-lower-wrapper">
+      <ul class="predict-list">
+        <li class="content-list">
+          <span class="bold">Plugin dependencies</span> — plugins can safely rely on other plugins, including the order in which they
+          must be execute, no matter the order in which you register them.
+        </li>
+        <li class="content-list">
+          <span class="bold">Extension priorities</span> — request lifecycle extensions can specify their relative order so future
+          extensions won’t disrupt the existing balance.
+        </li>
+        <li class="content-list">
+          <span class="bold">Namespace protection</span> — caches, plugins, decorators, and server methods are all protected and
+          cannot be implicitly overridden.
+        </li>
+        <li class="content-list">
+          <span class="bold">Zero routing conflicts</span> — paths will never conflict, and will always result in the same priority order —
+          no matter what order they’re added in.
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "noSide"
+};
+</script>
+
+<style lang="scss">
+@import "../assets/styles/main.scss";
+@import "../assets/styles/markdown.scss";
+
+.predict-middle-wrapper {
+  margin: 30px 0;
+}
+
+.predict-heading {
+  text-align: center;
+}
+</style>
