23b9a8a1a415ceed0642003b350d3bc3894dc272
jarrodyellets <jarrod524@gmail.com>
2019-06-07 14:21:49 +0200

    dynamic tutorial routes

---
+++ pages/tutorials/_tutorial.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <TutorialNav
+      :tutorial="tutorial"
+      :language="language"
+      @clicked="onClickChild"
+      @changed="onChangeChild"
+    />
+    <div class="tutorial-markdown-window">
+      <Tutorial :display="getPage" :language="language"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tutorial from "../../components/tutorials/Tutorial.vue";
+import TutorialNav from "../../components/tutorials/TutorialNav.vue";
+const page = require("../../static/lib/tutorials/");
+export default {
+  components: {
+    Tutorial,
+    TutorialNav
+  },
+  head() {
+    return {
+      title:
+        "hapi.js - " +
+        this.tutorial.replace(/([A-Z])/g, " $1").replace(/^./, function(str) {
+          return str.toUpperCase();
+        })
+    };
+  },
+  data() {
+    return {
+      tutorial: "gettingStarted",
+      language: "en_US"
+    };
+  },
+  computed: {
+    getPage() {
+      // /<ul>([^;]*)<\/ul>/
+      return this.$store.getters.loadPage;
+    }
+  },
+  methods: {
+    onClickChild(value) {
+      this.$data.tutorial = value.ref;
+      window.scrollTo(0, 0);
+    },
+    onChangeChild(value) {
+      this.$data.language = value;
+      this.$store.commit("setPage", page[value][this.tutorial].default);
+      window.scrollTo(0, 0);
+    }
+  },
+  created() {
+    this.$store.commit("setDisplay", "tutorials");
+    if(!page[this.language][this.$route.params.tutorial].default) {
+      error({statusCode: 404, message: "That tutorial does not exist!"})
+    } else {
+      this.$store.commit("setPage", page[this.language][this.$route.params.tutorial].default);
+    }
+    
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/styles/main.scss";
+@import "../../assets/styles/api.scss";
+</style>
+
--- pages/tutorials/index.vue
+++ pages/tutorials/index.vue
@@ -13,9 +13,9 @@
 </template>
 
 <script>
-import Tutorial from "~/components/tutorials/Tutorial.vue";
-import TutorialNav from "~/components/tutorials/TutorialNav.vue";
-const page = require("../static/lib/tutorials/");
+import Tutorial from "../../components/tutorials/Tutorial.vue";
+import TutorialNav from "../../components/tutorials/TutorialNav.vue";
+const page = require("../../static/lib/tutorials/");
 export default {
   components: {
     Tutorial,
@@ -44,7 +44,6 @@ export default {
   },
   methods: {
     onClickChild(value) {
-      this.$store.commit("setPage", page[this.language][value.ref].default);
       this.$data.tutorial = value.ref;
       window.scrollTo(0, 0);
     },
@@ -56,12 +55,19 @@ export default {
   },
   created() {
     this.$store.commit("setDisplay", "tutorials");
+    if (!page[this.language][this.$route.params.tutorial].default) {
+      error({ statusCode: 404, message: "That tutorial does not exist!" });
+    } else {
+      this.$store.commit(
+        "setPage",
+        page[this.language][this.$route.params.tutorial].default
+      );
+    }
   }
 };
 </script>
 
 <style lang="scss">
-@import "../assets/styles/main.scss";
-@import "../assets/styles/api.scss";
+@import "../../assets/styles/main.scss";
+@import "../../assets/styles/api.scss";
 </style>
-
