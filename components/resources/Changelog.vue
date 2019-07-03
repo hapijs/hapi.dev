b69a18d4d44f57061a861605230db2b1d835ddaa
jarrodyellets <jarrod524@gmail.com>
2019-07-03 15:34:15 +0200

    Fixed changelog links

--- components/resources/Changelog.vue
+++ components/resources/Changelog.vue
@@ -5,7 +5,7 @@
       v-for="change in milestones"
       v-bind:key="change[0].id"
       :version="change[0].milestone.title"
-      :versionUR="change[0].milestone.html_url"
+      :versionUrl="change[0].milestone.html_url"
       :issues="change"
     />
   </div>
