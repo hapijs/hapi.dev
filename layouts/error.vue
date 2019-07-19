d326b6e9bedf2ccc4d8594b9d0d14993fbfde81e
jarrodyellets <jarrod524@gmail.com>
2019-07-19 19:50:32 +0200

    404 Page

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,9 +2,10 @@
   <div class="error">
     <Nav/>
     <div class="error-container">
-        <h1>404!</h1>
-        <h2>Page not found</h2>
-        <nuxt-link to="/">Back to home page</nuxt-link>
+      <div class="error-heading">4<span><img class="error-helmet" src="../static/img/helmet.png" alt="hapi-helmet"></span>4</div>
+      <div class="error-description-heading">Oh No!</div>
+      <div class="error-description">It seems as though that page is not filled with joi today.</div>
+      <nuxt-link class="error-button" to="/">Back to the hapi place</nuxt-link>
     </div>
     <Footer/>
   </div>
@@ -23,7 +24,7 @@ export default {
     return {
       title: "hapi.js - Error!"
     };
-  },
+  }
 };
 </script>
 
@@ -38,7 +39,10 @@ export default {
 }
 
 .error-container {
-  margin: 50px auto 0 auto;
+  position: relative;
+  margin: 0 auto 0 auto;
+  padding-top: 30px;
+  box-sizing: border-box;
   width: 100%;
   max-width: 730px;
   display: flex;
@@ -47,6 +51,47 @@ export default {
   text-align: center;
   color: $black;
   font-size: 1.3em;
+}
+
+.error-heading {
+  position: relative;
+  font-size: 5rem;
+  color: $gray;
+}
+
+.error-description-heading {
+  font-size: 1.6rem;
+}
+
+.error-description {
+  position: relative;
+}
+
+.error-helmet {
+  width: 70px;
+  display: inline-block;
+}
+
+.error-button {
+  position: relative;
+  border-radius: 10px;
+  border: none;
+  background: $orange;
+  padding: 5px 15px;
+  font-size: 0.9em;
+  font-weight: 700;
+  margin: 40px 0;
+  color: #fff;
+  cursor: pointer;
+  border: 4px solid rgba(0, 0, 0, 0);
+}
+
+.error-button:hover {
+  border: 4px solid $orange;
+  background: #fff;
+  color: $orange;
+  text-decoration: none;
+  transition: all 0.3s ease 0s;
 }
 
 .footer {
@@ -71,8 +116,12 @@ export default {
   margin: 0;
 }
 
-@media screen and (max-width: 900px) {
+@keyframes drop {
+  0% {
+  }
+}
 
+@media screen and (max-width: 900px) {
   .logo {
     height: 50px;
     width: calc(100px * (2 / 3));
