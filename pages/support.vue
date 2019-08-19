48dba47b10702b4c20edd53588d4299d7686aadc
jarrodyellets <jarrod524@gmail.com>
2019-08-19 10:39:17 +0200

    Restructure

--- pages/support.vue
+++ pages/support.vue
@@ -1,19 +1,46 @@
 <template>
-  <div class="help-wrapper">
-    <div class="help-top">There are a few ways in which you can receive help using hapi:</div>
-    <div class="help-middle">
-      - For any issue or question (no matter how basic), open an issue on
-      <a
-        class="help-link"
-        href="https://github.com/hapijs/hapi"
-      >github</a>.
+  <div class="support-wrapper">
+    <h2 class="support-main-heading">How can we help?</h2>
+    <div class="support-content">
+      Got a question or a problem with your hapi application? Not to worry!
+      The
+      <span
+        class="underline"
+      >hapi Developer Portal</span> contains plenty of resources to help you get
+      started and use hapi. But when that’s not enough…
     </div>
-    <div class="help-bottom">
-      - For discussions, join the
+    <h4 class="support-minor-heading">Curious about commercial support plans?</h4>
+    <div class="support-content">
+      A hapi commercial license means you control your
+      upgrade timeline, access priority support, and build applications on a sustainable,
+      predictable, and secure foundation.
+      <span
+        class="bold"
+      >Plans start as low as $95/month</span>.
+    </div>
+    <div class="support-button-wrapper">
       <a
-        class="help-link"
-        href="https://join.slack.com/t/hapihour/shared_invite/enQtNTA5MDUzOTAzOTU4LTUyZmFiYjkyMTBmNDcyMmI2MmRjMzg4Y2YzNTlmNzUzNjViN2U1NmYyY2NjYjhiYWU4MGE2OTFhZDRlYWMyZDY"
-      >Slack channel</a>.
+        class="support-button"
+        href="#plans"
+        aria-label="plans-button"
+      >Choose the plan that fits your team</a>
+    </div>
+    <h4 class="support-minor-heading">No worries, we're here to help!</h4>
+    <div class="support-content">
+      <span class="bold">Free support</span> is always available on GitHub.
+      Just open an issue with your question and a community member will try to help. For faster response,
+      join the <span class="underline">hapi Slack channel</span> – it’s where many community members hang out and help each other.
+    </div>
+    <div class="security-content">
+      Like all open source projects, free support is constrained by the availability
+      of volunteer resources. When free support is not enough, commercial licensing and support plans are available.
+    </div>
+    <div class="support-button-wrapper">
+      <a
+        class="support-button"
+        href="#plans"
+        aria-label="github-button"
+      >Open an issue on GitHub</a>
     </div>
   </div>
 </template>
@@ -36,22 +63,55 @@ export default {
 @import "../assets/styles/main.scss";
 @import "../assets/styles/markdown.scss";
 
-.help-wrapper {
-  max-width: 1260px;
+.support-wrapper {
+  max-width: 1000px;
   width: 100%;
   margin: 0 auto;
   padding: 0 60px;
   font-size: 1.2rem;
 }
 
-.help-top,
-.help-middle {
-  margin-bottom: 25px;
+.support-main-heading {
+  text-align: center;
+  font-weight: 900;
+  margin: 40px 0;
 }
 
-.help-middle,
-.help-bottom {
-  margin-left: 25px;
+.support-minor-heading {
+  text-align: center;
+  font-weight: 900;
+  margin: 40px 0 0 0;
+}
+
+.support-content {
+  margin: 10px 0;
+}
+
+.support-button-wrapper {
+  margin: 30px auto 50px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.support-button {
+  border-radius: 10px;
+  border: none;
+  background: $orange;
+  padding: 7px 15px;
+  font-size: 0.9em;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
+  border: 4px solid rgba(0, 0, 0, 0);
+}
+
+.support-button:hover {
+  border: 4px solid $orange;
+  background: #fff;
+  color: $orange;
+  text-decoration: none;
+  transition: all 0.3s ease 0s;
 }
 
 @media screen and (max-width: 900px) {
