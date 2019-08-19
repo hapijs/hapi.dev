9a409eef84446077026fd3cbabaa66f7a138e498
jarrodyellets <jarrod524@gmail.com>
2019-08-19 10:43:23 +0200

    How to help

--- pages/support.vue
+++ pages/support.vue
@@ -1,53 +1,17 @@
 <template>
   <div class="support-wrapper">
-    <h2 class="support-main-heading">How can we help?</h2>
-    <div class="support-content">
-      Got a question or a problem with your hapi application? Not to worry!
-      The
-      <span
-        class="underline"
-      >hapi Developer Portal</span> contains plenty of resources to help you get
-      started and use hapi. But when that’s not enough…
-    </div>
-    <h4 class="support-minor-heading">Curious about commercial support plans?</h4>
-    <div class="support-content">
-      A hapi commercial license means you control your
-      upgrade timeline, access priority support, and build applications on a sustainable,
-      predictable, and secure foundation.
-      <span
-        class="bold"
-      >Plans start as low as $95/month</span>.
-    </div>
-    <div class="support-button-wrapper">
-      <a
-        class="support-button"
-        href="#plans"
-        aria-label="plans-button"
-      >Choose the plan that fits your team</a>
-    </div>
-    <h4 class="support-minor-heading">No worries, we're here to help!</h4>
-    <div class="support-content">
-      <span class="bold">Free support</span> is always available on GitHub.
-      Just open an issue with your question and a community member will try to help. For faster response,
-      join the <span class="underline">hapi Slack channel</span> – it’s where many community members hang out and help each other.
-    </div>
-    <div class="security-content">
-      Like all open source projects, free support is constrained by the availability
-      of volunteer resources. When free support is not enough, commercial licensing and support plans are available.
-    </div>
-    <div class="support-button-wrapper">
-      <a
-        class="support-button"
-        href="#plans"
-        aria-label="github-button"
-      >Open an issue on GitHub</a>
-    </div>
+    <howToHelp />
   </div>
 </template>
 
 <script>
+import howToHelp from "~/components/support/howToHelp.vue";
+
 export default {
   layout: "noSide",
+  components: {
+    howToHelp
+  },
   head() {
     return {
       title: "hapi.js - Help"
