bf3ab9ec7c12ed1773f364df38af787bea6e03f6
jarrodyellets <jarrod524@gmail.com>
2019-09-11 21:03:12 +0200

    Update New Support Page

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,7 +78,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/vue-js-modal'],
 
   /*
    ** Nuxt.js modules
--- pages/support.vue
+++ pages/support.vue
@@ -42,27 +42,145 @@ export default {
 @import "../assets/styles/main.scss";
 @import "../assets/styles/markdown.scss";
 
-.help-wrapper {
-  max-width: 1260px;
+.support-wrapper {
+  max-width: 1070px;
   width: 100%;
   margin: 0 auto;
-  padding: 0 60px;
   font-size: 1.2rem;
 }
-
-.help-top,
-.help-middle {
-  margin-bottom: 25px;
+.support-main-heading {
+  text-align: center;
+  font-weight: 900;
+  margin: 40px 0;
 }
-
-.help-middle,
-.help-bottom {
-  margin-left: 25px;
+.support-minor-heading {
+  text-align: center;
+  font-weight: 900;
+  margin: 40px 0 0 0;
 }
-
+.support-content {
+  margin: 10px 0;
+}
+.support-button-wrapper {
+  margin: 30px auto 80px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.support-button {
+  border-radius: 10px;
+  border: none;
+  background: $orange;
+  padding: 7px 15px;
+  font-size: 0.9em;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
+  border: 4px solid rgba(0, 0, 0, 0);
+}
+.support-button:hover {
+  border: 4px solid $orange;
+  background: #fff;
+  color: $orange;
+  text-decoration: none;
+  transition: all 0.3s ease 0s;
+}
+.modal-top {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal-logo {
+  width: 150px;
+}
+.modal-close {
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  width: 25px;
+}
+.modal-description {
+  margin: 0 auto;
+  width: 100%;
+  text-align: center;
+  padding: 10px 0;
+  border-bottom: 1px solid $dark-white;
+}
+.confirmation-text {
+  text-align: center;
+}
+.modal-label {
+  margin: 0;
+}
+.modal-input {
+  width: 100%;
+  height: 30px;
+  padding: 5px;
+  margin-bottom: 10px;
+}
+.modal-text {
+  width: 100%;
+  height: 125px;
+  margin-bottom: 20px;
+  resize: none;
+  padding: 5px;
+}
+.modal-middle {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 20px;
+}
+.modal-button {
+  border-radius: 10px;
+  border: none;
+  background: $orange;
+  padding: 5px 15px;
+  font-size: 0.9em;
+  font-weight: 700;
+  color: #fff;
+  cursor: pointer;
+  border: 4px solid rgba(0, 0, 0, 0);
+}
+.modal-button:hover {
+  border: 4px solid $orange;
+  background: #fff;
+  color: $orange;
+  text-decoration: none;
+  transition: all 0.3s ease 0s;
+}
+.modal-bottom {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid $dark-white;
+}
+.modal-helmet {
+  width: 40px;
+}
 @media screen and (max-width: 900px) {
-  .help-wrapper {
-    padding: 10px 20px;
+  .support-wrapper {
+    padding: 0 20px;
+    font-size: 1.1em;
+  }
+  .modal-input {
+    height: 30px;
+  }
+  .modal-text {
+    height: 75px;
+  }
+  .modal-logo {
+    width: 100px;
+  }
+  .modal-bottom {
+    display: none;
   }
 }
 </style>
