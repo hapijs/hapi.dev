e3bec704e609754f48a94ecf0b92490b2d18cf0b
jarrodyellets <jarrod524@gmail.com>
2019-09-20 11:48:54 +0200

    Merge table

--- pages/resources/status.vue
+++ pages/resources/status.vue
@@ -3,25 +3,70 @@
     <ResourcesNav page="status" />
     <div class="module-status-wrapper">
       <h1 class="module-status-header">Module Status</h1>
-      <ModuleStatus
-        v-for="repo in orderedRepos"
-        v-bind:key="repo.name"
-        :name="repo.name"
-        :versions="repo.versions"
-      />
+      <div class="module-status-table-wrapper">
+        <table class="status-table">
+          <thead>
+            <tr class="header-row">
+              <th class="header-module">Module</th>
+              <th class="version-header">Version</th>
+              <th class="dependencies-header">Dependencies</th>
+              <th class="travis-header">Travis Build</th>
+              <th class="license-header">License</th>
+              <th class="life-header">End of Life</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="repo in orderedRepos" v-bind:key="repo.name" class="module-row">
+              <td class="module-name">{{repo.name}}</td>
+              <td colspan="5" class="nested-td">
+                <table class="nested-table">
+                  <tbody class="nested-tbody">
+                    <tr v-for="version in repo.versions" v-bind:key="version.name">
+                      <td>
+                        <a
+                          target="__blank"
+                          class="status-link"
+                          :href='apiModules.includes(repo.name) ? "/family/" + repo.name + "/?v=" + version.name : "https://github.com/hapijs/" + repo.name + "/tree/" + version.branch'
+                        >{{version.name}}</a>
+                      </td>
+                      <td class="status-badge">
+                        <!-- <svg viewBox="0 0 100 100"><use :xlink:href='"https://david-dm.org/hapijs/" + repo.name + ".svg?branch=" + version.branch'></use></svg> -->
+                        <object :data='"https://david-dm.org/hapijs/" + repo.name + ".svg?branch=" + version.branch' :id='"dependSVG-" + repo.name'></object>
+                        <!-- <img
+                          :src='"https://david-dm.org/hapijs/" + repo.name + ".svg?branch=" + version.branch'
+                          alt="Dependency Status"
+                        /> -->
+                      </td>
+                      <td class="status-badge">
+                        <img
+                          :src='"https://travis-ci.org/hapijs/" + repo.name + ".svg?branch=" + version.branch'
+                          alt="Build Status"
+                        />
+                      </td>
+                      <td>{{version.license}}</td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+              <!-- <td>{{life.endOfLife[camelName][version.name] && life.endOfLife[camelName][version.name]}}</td> -->
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import ResourcesNav from "../../components/resources/ResourcesNav.vue";
-import ModuleStatus from "../../components/resources/ModuleStatus.vue";
+const life = require("../../static/lib/endOfLife.js");
 let Semver = require("semver");
+import _ from 'lodash';
 
 export default {
   components: {
-    ResourcesNav,
-    ModuleStatus
+    ResourcesNav
   },
   head() {
     return {
@@ -37,7 +82,20 @@ export default {
   },
   data() {
     return {
-      page: "status"
+      page: "status",
+      apiModules: [
+        "bell",
+        "boom",
+        "good",
+        "hoek",
+        "iron",
+        "joi",
+        "shot",
+        "topo",
+        "yar"
+      ],
+      life: life,
+      camelName: _.camelCase(this.name)
     };
   },
   async asyncData({ params, $axios }) {
@@ -143,6 +201,13 @@ export default {
   },
   async created() {
     await this.$store.commit("setDisplay", "resources");
+  },
+  mounted() {
+    this.$nextTick(function () {
+      let textSVG = document.querySelectorAll("object")
+      console.log(textSVG)
+    })
+  
   }
 };
 </script>
@@ -161,10 +226,86 @@ export default {
   width: 100%;
 }
 
+.module-status-table-wrapper {
+  margin: 0;
+  width: 100%;
+}
+
 .module-status-header {
   margin: 20px 0 10px;
   border-bottom: 1px solid #ddd;
   border-top: none;
   padding-bottom: 10px;
+}
+
+.status-table {
+  width: 100%;
+}
+
+.header-row {
+  width: 100%
+}
+
+.module-header {
+  width: 20%;
+}
+
+.version-header, .dependencis-header, .travis-header, .license-header, .life-header {
+  width: 16%;
+}
+
+.module-name {
+  width: 20%;
+  border-right: 1px solid $dark-white;
+  text-align: center;
+  vertical-align: middle;
+  font-size: 1.2em;
+  font-weight: 700;
+}
+
+.module-row {
+  border-bottom: 1px solid $dark-white;
+  background-color: #fff;
+}
+
+.module-row:nth-child(odd) {
+  background-color: $off-white;
+}
+
+.nested-table {
+  width: 100%;
+}
+
+.nested-table td {
+  width: 20%;
+}
+
+.status-table th {
+  padding: 0 10px;
+}
+
+.status-table td {
+  padding: 10px;
+}
+
+.status-table tbody {
+  border: 1px solid $dark-white;
+}
+
+.nested-tbody {
+  border: none !important;
+}
+
+.nested-td {
+  box-sizing: border-box;
+  padding: 0 !important;
+}
+
+.nested-tbody tr:not(:last-child) {
+  border-bottom: 1px solid $dark-white;
+}
+
+.status-link {
+  font-weight: 700;
 }
 </style>
