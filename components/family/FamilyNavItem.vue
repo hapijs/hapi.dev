5fb8fe150f2b30322136c4dc5f1c50d632895e8b
jarrodyellets <jarrod524@gmail.com>
2019-10-04 14:36:37 +0200

    Update fam nav item

--- components/family/FamilyNavItem.vue
+++ components/family/FamilyNavItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li
-    :class="this.$route.params.family === name || (!this.$route.params.family && name === 'bell') ? 'side-nav-select-link ecosystem-side-nav-item side-nav-active' : 'side-nav-select-link ecosystem-side-nav-item'"
+    :class="this.$route.params.family === name || (!this.$route.params.family && name === 'bell') ? 'side-nav-select-link family-side-nav-item side-nav-active' : 'side-nav-select-link family-side-nav-item'"
     :ref="name"
   >
     <a :href="'/family/' + name">{{name}}</a>
@@ -14,7 +14,6 @@
     </span>
     <div
       v-if="this.$props.active[name]"
-
       class="family-nav-select-wrapper family-display"
       :id='name'
       :key='version'
@@ -41,42 +40,41 @@ export default {
 <style lang="scss">
 @import "../../assets/styles/sideNav.scss";
 
-.ecosystem-side-nav-item {
+.family-side-nav-item {
   margin: 0 !important;
 }
 
-.ecosystem-side-nav-item:hover {
+.family-side-nav-item:hover {
   text-decoration: none !important;
 }
 
-.ecosystem-nav-select-wrapper {
+.family-nav-select-wrapper {
   display: none;
   width: 100%;
   font-weight: 400;
   margin: 0 0 10px 10px;
 }
 
-.ecosystem-nav-select-wrapper li {
+.family-nav-select-wrapper li {
   list-style-type: none;
   text-decoration: none;
   margin: 0;
   color: $gray;
 }
 
-.ecosystem-nav-select-wrapper ul {
+.family-nav-select-wrapper ul {
   margin: 0;
 }
 
-.ecosystem-nav-select-wrapper li ul {
+.family-nav-select-wrapper li ul {
   margin-left: 10px;
 }
 
-.ecosystem-nav-select-wrapper ul li {
+.family-nav-select-wrapper ul li {
   color: $gray !important;
   margin: 0;
   list-style-type: none;
 }
-
 
 .family-display {
   display: block;
