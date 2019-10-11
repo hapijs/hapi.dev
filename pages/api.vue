9fe98d58f77a415edb058afcbb2b3d04c5882e66
jarrodyellets <jarrod524@gmail.com>
2019-10-11 06:56:21 +0200

    Fix api search

--- pages/api.vue
+++ pages/api.vue
@@ -102,12 +102,12 @@ export default {
       for (let page of pages) {
         if (
           headers.indexOf(page.nodeName) !== -1 &&
-          page.innerHTML.indexOf(this.search.toLowerCase()) !== -1
+          page.innerHTML.toLowerCase().indexOf(this.search.toLowerCase()) !== -1
         ) {
           headlines.push(page);
         } else if (
           headers.indexOf(page.nodeName) === -1 &&
-          page.innerHTML.indexOf(this.search.toLowerCase()) !== -1
+          page.innerHTML.toLowerCase().indexOf(this.search.toLowerCase()) !== -1
         ) {
           text.push(page);
         }
