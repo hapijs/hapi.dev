248a0692b68383ec37baee1976bef62c8496bb07
jarrodyellets <jarrod524@gmail.com>
2019-11-18 10:22:15 +0100

    Add release notes icon

--- components/resources/ChangelogVersion.vue
+++ components/resources/ChangelogVersion.vue
@@ -2,8 +2,8 @@
   <div class="logWrapper">
     <div class="logVersionWrapper">
       <a :href="versionUrl" target="__blank" class="log-title">{{ version }}</a>
+      <a :href=releaseURL v-if=release class="releaseLink" target="_blank"><div class="release"><img class="releaseNotesImg" src="/img/release-notes.png" alt="Release Notes" title="View Release Notes">additional information</div></a>
       <div v-if=breaking class="breaking">breaking changes</div>
-      <div v-if=release class="release">Additional Information</div>
     </div>
     <div class="changelogtext-wrapper">
       <ChangelogText
@@ -29,7 +29,8 @@ export default {
   data() {
     return {
       breaking: false,
-      release: false
+      release: false,
+      releaseURL: ""
     };
   },
   created() {
@@ -39,6 +40,7 @@ export default {
       }
       if (issue.labels.some(label => label.name === 'release notes')) {
         this.$data.release = true;
+        this.$data.releaseURL = issue.html_url
       }
     }
   }
@@ -60,22 +62,39 @@ export default {
 
 .logVersionWrapper {
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   margin: 0;
 }
 
 .breaking {
-  padding: 1px 4px;
-  margin: 10px 10px 0 10px;
-  // border: 1px solid $dark-white;
+  padding: 2px 4px;
   font-size: .8em;
   font-weight: 600;
   background: $orange
 }
 
+.releaseLink, .releaseLink:hover {
+  color: $black;
+}
+
+.release {
+  display: flex;
+  align-items: center;
+  margin: 0 10px 0 0;
+  padding: 2px 4px;
+  font-size: .8em;
+  font-weight: 600;
+  background: #fad8c7;
+}
+
+.releaseNotesImg {
+  width: 15px;
+  margin-right: 5px;
+}
+
 .log-title {
   font-size: 1.75rem;
   color: $orange;
-  margin: 0;
+  margin: 0 10px 0 0;
 }
 </style>
