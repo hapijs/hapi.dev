e6ad9c8999ed4f7a5ecb7aea2e6c15488731d38f
jarrodyellets <jarrod524@gmail.com>
2019-11-26 13:35:48 +0100

    Revert fam

--- pages/family/index.vue
+++ pages/family/index.vue
@@ -14,17 +14,17 @@
         <div
           class="family-grid-cell"
           :id="module.name"
-          v-for="module in sorted"
+          v-for="module in moduleData"
           v-bind:key="module.name"
         >
           <div class="family-grid-text-wrapper">
-            <a
-              :href="'/family/' + module.name"
-              class="family-grid-link"
-            >
+            <a :href="'/family/' + module.name" class="family-grid-link">
               <div class="family-grid-cell-name">{{ module.name }}</div>
             </a>
-            <div class="family-grid-cell-slogan" v-html="$md.render(module.slogan)"></div>
+            <div
+              class="family-grid-cell-slogan"
+              v-html="$md.render(module.slogan)"
+            ></div>
           </div>
           <div class="family-grid-cell-stats">
             <div class="stats-wrapper">
@@ -46,9 +46,7 @@
                 {{ module.forks }}
               </div>
             </div>
-            <div class="family-updated">
-              Updated: {{ module.updated }}
-            </div>
+            <div class="family-updated">Updated: {{ module.updated }}</div>
           </div>
         </div>
       </div>
@@ -58,8 +56,6 @@
 
 <script>
 import FamilyIndexNav from "~/components/family/FamilyIndexNav.vue";
-import moduleInfo from "../../static/lib/moduleInfo.json";
-
 export default {
   components: {
     FamilyIndexNav
@@ -67,9 +63,7 @@ export default {
   data() {
     return {
       modules: this.$store.getters.loadModules,
-      moduleData: moduleInfo,
       search: "",
-      sorted: [],
       core: true,
       sort: "name"
     };
@@ -113,18 +107,57 @@ export default {
     sortModules(value) {
       this.$data.sort = value;
       if (value === "stars" || value === "forks") {
-        this.sorted.sort((a, b) =>
+        this.moduleData.sort((a, b) =>
           a[value.toLowerCase()] < b[value] ? 1 : -1
         );
       } else if (value === "name") {
-        this.sorted.sort((a, b) => (a.name > b.name ? 1 : -1));
+        this.moduleData.sort((a, b) => (a.name > b.name ? 1 : -1));
       } else if (value === "updated") {
-        this.sorted.sort((a, b) => (a.date < b.date ? 1 : -1));
+        this.moduleData.sort((a, b) => (a.date < b.date ? 1 : -1));
       }
     }
   },
+  async asyncData({ params, $axios, route, store }) {
+    const options = {
+      headers: {
+        accept: "application/vnd.github.v3.raw+json",
+        authorization: "token " + process.env.GITHUB_TOKEN
+      }
+    };
+    let moduleData = [];
+    for (let module of store.getters.loadModules) {
+      try {
+        let readme = await $axios.$get(
+          "https://api.github.com/repos/hapijs/" +
+            module +
+            "/contents/README.md",
+          options
+        );
+        let forks = await $axios.$get(
+          "https://api.github.com/repos/hapijs/" + module,
+          options
+        );
+        let slogan =
+          (await readme.match(/####(.*)/gm)) !== null
+            ? await readme.match(/####(.*)/gm)[0].substring(5)
+            : "Description coming soon...";
+        let date = await new Date(forks.pushed_at);
+        moduleData.push({
+          name: module,
+          slogan: await slogan,
+          forks: await Number(forks.forks_count),
+          stars: await Number(forks.stargazers_count),
+          date: await forks.pushed_at,
+          updated: await date.toDateString(),
+          link: "https://github.com/hapijs/" + module
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    return { moduleData };
+  },
   created() {
-    this.$data.sorted = Object.values(this.moduleData).filter(item => this.$data.modules.includes(item.name))
     this.$store.commit("setDisplay", "family");
     const sortedBy = ["name", "stars", "forks", "updated"];
     if (sortedBy.includes(this.$route.query.sort)) {
@@ -147,23 +180,19 @@ export default {
 <style lang="scss">
 @import "../../assets/styles/main.scss";
 @import "../../assets/styles/api.scss";
-
 .family-grid-wrapper {
   padding: 20px 100px;
   width: 100%;
 }
-
 .family-header {
   margin-bottom: 30px;
   font-size: 2rem;
 }
-
 .family-grid {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
   grid-gap: 20px;
 }
-
 .family-grid-cell {
   position: relative;
   display: flex;
@@ -175,29 +204,23 @@ export default {
   margin: 0;
   background: $off-white;
 }
-
 .family-grid-text-wrapper {
   margin: 0;
 }
-
 .family-grid-cell-name {
   color: $orange;
   font-size: 1.2em;
   font-weight: 700;
 }
-
 .family-grid-link:hover {
   color: $orange;
 }
-
 .family-grid-cell-slogan {
   font-size: 0.95em;
 }
-
 .family-grid-cell-slogan p {
   margin: 0;
 }
-
 .family-grid-cell-stats {
   position: relative;
   bottom: 0;
@@ -207,65 +230,53 @@ export default {
   margin: 10px 0 0 0;
   width: 100%;
 }
-
 .stats-wrapper {
   display: flex;
   align-items: center;
   margin: 0;
   font-size: 0.9em;
 }
-
 .family-stats {
   display: flex;
   align-items: center;
 }
-
 .family-stats:nth-child(-n + 2) {
   padding-right: 20px;
 }
-
 .status-link {
   display: inline-block;
   height: 18px;
   margin: 0;
 }
-
 .stats-img-github {
   height: 18px;
 }
-
 .stats-img-star {
   height: 16px;
   margin-right: 5px;
 }
-
 .stats-img-fork {
   height: 14px;
   margin-right: 5px;
 }
-
 .family-updated {
   font-size: 0.8em;
   font-style: italic;
   justify-self: flex-end;
   margin: 0;
 }
-
 @media screen and (max-width: 1500px) {
   .family-grid-wrapper {
     padding: 20px 40px;
   }
 }
-
 @media screen and (max-width: 900px) {
   .family-grid-wrapper {
     padding: 20px;
   }
-
   .family-grid {
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   }
-
   .family-grid-cell-slogan p {
     font-size: 0.97em;
   }
