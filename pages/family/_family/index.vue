66a73c216898488231bcb4f1343786f23e739087
jarrodyellets <jarrod524@gmail.com>
2019-12-10 12:36:03 +0100

    Remove console.log

--- pages/family/_family/index.vue
+++ pages/family/_family/index.vue
@@ -192,7 +192,6 @@ export default {
     }
   },
   created() {
-    console.log(moduleInfo)
     this.$data.modules = moduleInfo;
     let versionsArray = moduleInfo[this.$route.params.family].versionsArray;
     if (!this.$store.getters.loadModules.includes(this.$route.params.family)) {
