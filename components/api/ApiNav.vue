737e3b8fbda3fade86c0614e195748bc6dbf6dd2
jarrodyellets <jarrod524@gmail.com>
2019-12-13 12:59:49 +0100

    API sticky nav

--- components/api/ApiNav.vue
+++ components/api/ApiNav.vue
@@ -2,43 +2,58 @@
   <div class="api-nav-window">
     <div class="api-nav-wrapper">
       <div class="api-nav-inner-wrapper">
-        <div class="api-nav-title-wrapper">
-          <div class="api-nav-title">API</div>
-          <div class="api-lang-wrapper">
-            <select @change="onChange($event)" class="api-lang-select" :value="version">
-              <option
-                    v-for="version in versions"
-                    v-bind:key="version"
-                    :value="version"
-                  >{{version}}</option>
-            </select>
+        <div class="api-header-wrapper">
+          <div class="api-nav-title-wrapper">
+            <div class="api-nav-title">API</div>
+            <div class="api-lang-wrapper">
+              <select
+                @change="onChange($event)"
+                class="api-lang-select"
+                :value="version"
+              >
+                <option
+                  v-for="version in versions"
+                  v-bind:key="version"
+                  :value="version"
+                  >{{ version }}</option
+                >
+              </select>
+            </div>
           </div>
-        </div>
-        <div class="api-search">
-          <input
-            class="api-search-box"
-            :value="search"
-            v-on:keyup.enter="onSearch"
-            @input="onInput($event)"
-            placeholder="Search API"
-          />
-          <div class="api-search-img" v-on:click="onSearch"></div>
-          <div class="api-search-info">
-            <div class="api-search-results">
-              <div class="api-search-results-wrapper">
-                <div
-                  class="api-search-results-text"
-                >Showing result {{indexResults + 1}} of {{results.length}}</div>
-                <div class="api-search-buttons">
-                  <button class="api-search-button" v-on:click="onPrevious">Previous</button>
-                  <button class="api-search-button" v-on:click="onNext">Next</button>
+          <div class="api-search">
+            <input
+              class="api-search-box"
+              :value="search"
+              v-on:keyup.enter="onSearch"
+              @input="onInput($event)"
+              placeholder="Search API"
+            />
+            <div class="api-search-img" v-on:click="onSearch"></div>
+            <div class="api-search-info">
+              <div class="api-search-results">
+                <div class="api-search-results-wrapper">
+                  <div class="api-search-results-text">
+                    Showing result {{ indexResults + 1 }} of
+                    {{ results.length }}
+                  </div>
+                  <div class="api-search-buttons">
+                    <button class="api-search-button" v-on:click="onPrevious">
+                      Previous
+                    </button>
+                    <button class="api-search-button" v-on:click="onNext">
+                      Next
+                    </button>
+                  </div>
                 </div>
               </div>
+              <div class="api-search-error">No results found</div>
             </div>
-            <div class="api-search-error">No results found</div>
           </div>
         </div>
-        <div class="api-nav-select-wrapper" v-html="$md.render(this.$props.menu)"></div>
+        <div
+          class="api-nav-select-wrapper"
+          v-html="$md.render(this.$props.menu)"
+        ></div>
       </div>
       <div class="api-side-footer-wrapper">
         <SideFooter />
@@ -49,7 +64,6 @@
 
 <script>
 import SideFooter from "~/components/Footers/SideFooter.vue";
-
 export default {
   components: {
     SideFooter
@@ -58,7 +72,7 @@ export default {
   data: function() {
     return {
       uls: {},
-      links: {}
+      links: {},
     };
   },
   methods: {
@@ -137,11 +151,11 @@ export default {
           link.classList.add("api-nav-plus");
         }
         link.addEventListener("click", function(event) {
-          let currentActive = document.querySelector(".api-active")
+          let currentActive = document.querySelector(".api-active");
           if (currentActive) {
-            currentActive.classList.remove("api-active")
+            currentActive.classList.remove("api-active");
           }
-          link.classList.add("api-active")
+          link.classList.add("api-active");
           if (
             link.parentElement.children[1] &&
             link.parentElement.children[1].classList.contains("nav-display")
@@ -187,7 +201,6 @@ export default {
       for (let p of plus) {
         methods.push(p);
       }
-
       //Remove extra text from TOC elements
       for (let i = code.length - 1; i >= 0; i--) {
         code[i].classList.add("api-nav-code");
@@ -225,7 +238,6 @@ export default {
           let b = code[i + 1].innerHTML
             .replace(/\(([^#/(/)]+)\)/g, "()")
             .replace("await ", "");
-
           if (a === b) {
             continue;
           }
@@ -249,7 +261,6 @@ export default {
           );
         }
       }
-
       //API nav scroll spy
       let tags = document.querySelectorAll(".markdown-wrapper a");
       let points = {};
@@ -272,10 +283,7 @@ export default {
           offsets.push(tags[i].offsetTop - 40);
         }
       }
-
-
       let that = this;
-
       //Add active class to elements on scroll
       window.onscroll = function() {
         let location = document.documentElement.scrollTop;
@@ -287,9 +295,7 @@ export default {
         for (i in offsets) {
           aClass = points[offsets[i]].name;
           let element = document.querySelector(`a[href='${aClass}']`);
-          if (
-            (offsets[i] <= location || offsets[i] <= locationBody)
-          ) {
+          if (offsets[i] <= location || offsets[i] <= locationBody) {
             for (let a of actives) {
               a.classList.remove("api-active");
             }
@@ -301,10 +307,6 @@ export default {
         }
         if (active) {
           let activeClass;
-          let bottom = active.getBoundingClientRect().bottom;
-          if (bottom > window.innerHeight) {
-            active.scrollIntoView(false);
-          }
           if (that.$route.hash === active.hash && bottom === 0) {
             let wrapperHeight = document
               .querySelector(".api-nav-wrapper")
@@ -328,6 +330,11 @@ export default {
                 "api-nav-minus"
               );
             }
+          }
+          let bottom = active.getBoundingClientRect().bottom;
+          let location = active.getBoundingClientRect();
+          if (bottom > window.innerHeight || location.top < 131) {
+            active.scrollIntoView(false);
           }
           if (that.$route.hash === active.hash && bottom === 0) {
             active.scrollIntoView(false);
@@ -370,7 +377,6 @@ export default {
 <style lang="scss">
 @import "../../assets/styles/sideNav.scss";
 @import "../../assets/styles/main.scss";
-
 .api-nav-window {
   position: sticky;
   top: 96px;
@@ -382,7 +388,7 @@ export default {
   overflow-y: auto;
   -webkit-font-smoothing: auto;
   -moz-osx-font-smoothing: auto;
-  padding: 20px 0 5px 0;
+  padding: 0 0 5px 0;
   margin: 0;
   display: flex;
   flex-direction: column;
@@ -391,7 +397,6 @@ export default {
   background: $off-white;
   border-right: 1px solid $dark-white;
 }
-
 .api-nav-wrapper {
   display: flex;
   flex-direction: column;
@@ -401,16 +406,15 @@ export default {
   height: auto;
   min-height: calc(100vh - 122px);
 }
-
 .api-nav-inner-wrapper {
   display: flex;
   flex: 0 0 auto;
   flex-direction: column;
   align-items: flex-start;
   margin: 0;
-  overflow-x: hidden;
+  // overflow-x: hidden;
+  position: relative;
 }
-
 .api-nav-title-wrapper {
   padding: 0 20px 20px 20px;
   width: 100%;
@@ -418,20 +422,26 @@ export default {
   justify-content: flex-start;
   align-items: flex-end;
 }
-
+.api-header-wrapper {
+  margin: 0;
+  width: 100%;
+  top: 0;
+  position: sticky;
+  z-index: 1000;
+  padding: 20px 0;
+  background: $off-white;
+}
 .api-nav-title {
   font-size: 1.5rem;
   color: $black;
   margin: 0;
 }
-
 .api-lang-wrapper {
   display: flex;
   justify-content: flex-start;
   align-items: flex-end;
   margin: 0 0 0 10px;
 }
-
 .api-lang-select {
   width: 70px;
   padding: 0px 5px 0px 5px;
@@ -444,13 +454,11 @@ export default {
   background: url(/img/down.png) 96% / 15% no-repeat $off-white;
   cursor: pointer;
 }
-
 .api-search {
   position: relative;
   padding: 0 20px;
   width: 100%;
 }
-
 .api-search-box {
   height: 30px;
   outline: none;
@@ -461,7 +469,6 @@ export default {
   border-radius: 0;
   -webkit-appearance: none;
 }
-
 .api-search-img {
   position: absolute;
   background: url("/img/search.png") no-repeat;
@@ -479,34 +486,28 @@ export default {
   z-index: 25;
   cursor: pointer;
 }
-
 .api-search-info {
   margin-top: 5px;
   width: 100%;
 }
-
 .api-search-error,
 .api-search-results {
   display: none;
   width: 100%;
   font-size: 0.85em;
 }
-
 .api-search-results-wrapper {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
 }
-
 .api-search-results-text {
   margin: 0;
 }
-
 .api-search-buttons {
   margin: 5px 0 0 0;
 }
-
 .api-search-button {
   outline: none;
   border: 1px solid $dark-white;
@@ -516,72 +517,59 @@ export default {
   cursor: pointer;
   margin-right: 10px;
 }
-
 .api-nav-select-wrapper ul {
   margin-left: 0;
 }
-
 .api-nav-select-wrapper {
-  margin-top: 20px;
+  margin: 0;
   font-size: 1.1em;
   color: $orange;
   line-height: 30px;
   width: 100%;
+  overflow-x: hidden;
 }
-
 .api-nav-ul {
   display: none;
 }
-
 .ul-block {
   display: block;
 }
-
 .api-nav-select-wrapper ul li ul {
   margin-left: 10px;
 }
-
 .api-nav-li {
   position: relative;
   color: $gray;
 }
-
 .api-nav-header {
   color: $gray;
   font-size: 1rem;
   width: 100%;
   padding: 2px 0;
 }
-
 .api-nav-header:hover {
   color: $gray;
 }
-
 .api-active:hover {
   color: #fff;
 }
-
 .api-nav-plus:hover,
 .api-nav-minus:hover {
   color: $orange;
 }
-
 .api-nav-header * {
   text-decoration: none;
 }
-
 .api-nav-li a:hover,
 .api-nav-li a code:hover {
   text-decoration: underline;
 }
-
 .api-nav-plus,
 .api-nav-minus,
 .api-nav-plus code,
 .api-nav-minus code {
   color: $orange;
 }
-
 .api-nav-plus:after {
   content: "\002B";
   display: flex;
@@ -596,12 +584,10 @@ export default {
   width: 15px;
   z-index: 100;
 }
-
 .api-nav-select-wrapper > ul > .api-nav-li > .api-nav-plus:after,
 .api-nav-select-wrapper > ul > .api-nav-li > .api-nav-minus:after {
   height: 27px;
 }
-
 .api-nav-minus:after {
   content: "\2212";
   color: inherit;
@@ -617,11 +603,9 @@ export default {
   width: 15px;
   z-index: 100;
 }
-
 .api-nav-header {
   display: flex;
 }
-
 .api-nav-code {
   background: $off-white;
   font-family: "Lato", sans-serif;
@@ -631,45 +615,37 @@ export default {
   padding: 0;
   border: none;
 }
-
 .api-nav-select-wrapper ul {
   margin: 0 0 0 30px;
 }
-
 .api-nav-select-wrapper ul li {
   margin: 0;
   list-style-type: none;
 }
-
 .api-active,
 .api-active * {
   position: relative;
   color: #fff !important;
   background: $gray;
 }
-
 .api-bold {
   font-weight: 900;
 }
-
 .api-active:after {
   position: absolute;
   left: 43px;
   height: 27px;
 }
-
 .api-active {
   display: inline-block;
   left: -60px;
   padding: 2px 30px 2px 60px !important;
   width: 375px !important;
 }
-
 .api-side-footer-wrapper {
   width: 100%;
   padding: 0 20px;
 }
-
 @media screen and (max-width: 900px) {
   .api-nav-window {
     flex-direction: row;
@@ -685,7 +661,6 @@ export default {
     min-height: auto;
   }
 }
-
 @media print {
   .api-nav-window {
     display: none;
