514198388ba96fc76d2eab7802fbf4606c656cb3
jarrodyellets <jarrod524@gmail.com>
2019-12-18 11:20:51 +0100

    Show validation

--- pages/family/_family/tester.vue
+++ pages/family/_family/tester.vue
@@ -3,10 +3,15 @@
     <LandingNav
 
     />
-    <div class="markdown-wrapper">
+    <div class="test-wrapper">
       <h1 class="tester-title">joi Tester</h1>
       <h2 class="tester-subTitle">Schema</h2>
       <codemirror :value="schema" :options="options" @input="onSchemaChange"></codemirror>
+      <h2 class="tester-subTitle">Data To Validate</h2>
+      <codemirror :value="validate" :options="options" @input="onValidateChange"></codemirror>
+      <button class="validate-button" v-on:click="onValidateClick">Validate</button>
+      <h2 class="tester-subTitle">Result</h2>
+      <pre>{{result}}</pre>
     </div>
   </div>
 </template>
@@ -39,14 +44,17 @@ export default {
   data() {
     return {
       schema:
-        '//Insert your joi schema here:\n' +
         'Joi.object({\n' +
         '  username: Joi.string().alphanum().min(3).max(30).required(),\n' +
-        '  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),\n' +
+        '  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),\n' +
+        '  repeat_password: Joi.ref("password"),\n' +
         '  access_token: [Joi.string(), Joi.number()],\n' +
-        '  birthyear: Joi.number().integer().min(1900).max(2013),\n' +
+        '  birth_year: Joi.number().integer().min(1900).max(2013),\n' +
         '  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } } )\n' +
-        '}).with(\'username\', \'birthyear\').xor(\'password\', \'access_token\').with(\'password\', \'repeat_password\')',
+        '}).with(\'username\', \'birth_year\').xor(\'password\', \'access_token\').with(\'password\', \'repeat_password\')',
+      validate:
+        '{ username: "abc", password: "password", repeat_password: "password", birth_year: 1994 }',
+      result: '',
       options: {
         theme: 'eclipse',
         tabSize: 2,
@@ -59,7 +67,54 @@ export default {
   methods: {
     onSchemaChange(input) {
       this.schema = input
+    },
+    onValidateChange(input) {
+      this.validate = input
+    },
+    onValidateClick() {
+      this.result = "";
+      try {
+        let validatedObject = Function('"use strict";return (' + this.validate + ')')();
+        let joiSchema = new Function('Joi', '"use strict";return (' + this.schema + ')');
+        let validatedResults = joiSchema(Joi).validate(validatedObject)
+        if (validatedResults.error) {
+          this.result = validatedResults.error.toString()
+        } else {
+          this.result = "Validation Passed"
+        }
+      } catch (error) {
+        this.result = error.toString()
+      }
     }
+  },
+  created() {
+    const schema1 = Joi.object({
+    username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
+    password: Joi.string(),
+
+    repeat_password: Joi.ref('password'),
+
+    access_token: [
+        Joi.string(),
+        Joi.number()
+    ],
+
+    birth_year: Joi.number()
+        .integer()
+        .min(1900)
+        .max(2013),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+})
+    .with('username', 'birth_year')
+    .xor('password', 'access_token')
+    .with('password', 'repeat_password');
   }
 }
 </script>
@@ -68,6 +123,12 @@ export default {
 @import "../../../assets/styles/main.scss";
 @import 'codemirror/lib/codemirror.css';
 @import 'codemirror/theme/eclipse.css';
+
+.test-wrapper {
+  width: 100%;
+  margin: 0;
+  padding: 20px 40px;
+}
 
 .tester-title {
   font-size: 2rem;
@@ -87,6 +148,7 @@ export default {
   overflow-y: auto;
   font-family: inconsolata,menlo,consolas,monospace !important;
   height: 100% !important;
+  margin-bottom: 40px;
 }
 
 .CodeMirror-sizer {
